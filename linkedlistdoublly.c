bf13bf923f140b1c1f324270ab8ff6eafdd74942
Meghahullatti <meghachanabasappa.cs23@bmsce.ac.in>
2024-11-20 10:33:59 +0530

    linkedlistdoublly.c

---
+++ linkedlistdoublly.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+void findMiddle(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+
+   
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;     
+        fast = fast->next->next; 
+    }
+
+    
+    if (slow != NULL) {
+        printf("The middle element is: %d\n", slow->data);
+    }
+}
+
+
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+
+int main() {
+   
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    
+    printf("Linked List: ");
+    printList(head);
+
+    
+    findMiddle(head);
+
+    return 0;
+}
+
